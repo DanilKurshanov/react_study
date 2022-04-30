e477b7f5974887425b9ec1fe393b709ad2e0468d
KurshanovDanil <kurshanov.danil@gmail.com>
2022-04-30 20:58:43 +0300

    create Contex for User

--- shop-clothing/src/components/sign-in-form/sign-in-form.component.jsx
+++ shop-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -8,6 +8,8 @@ import {
     signInAuthUserWithEmailAndPassword,
     signInWithGooglePopup
 } from "../../utils/firebase/firebase.utils";
+
+import {UserContext} from "../../contexts/user.context";
 
 import './sign-in-form.styles.scss'
 
@@ -21,12 +23,16 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {email, password} = formFields;
 
+    const { setCurrentUser } = useContext(UserContext)
+
     const handlerSubmit = async (event) => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
             resetFormFields();
+
+            setCurrentUser(user)
         } catch (error) {
             switch (error.code) {
                 case 'auth/wrong-password':
--- shop-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ shop-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -9,6 +9,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import './sign-up-form.styles.scss'
+import {UserContext} from "../../contexts/user.context";
 
 const defaultFormFields = {
     displayName: '',
@@ -23,6 +24,8 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const {setCurrentUser } = useContext(UserContext);
+
     const handlerSubmit = async (event) => {
         event.preventDefault();
 
@@ -32,6 +35,8 @@ const SignUpForm = () => {
         }
         try {
             const {user} = await createAuthUserWithEmailAndPassword(email, password);
+
+            setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
