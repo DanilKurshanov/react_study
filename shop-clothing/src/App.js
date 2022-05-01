4f589d109a6026f75933e123f176577127979934
KurshanovDanil <kurshanov.danil@gmail.com>
2022-05-01 21:34:43 +0300

    start create Shop Page

--- shop-clothing/src/App.js
+++ shop-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Outlet} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import HomePage from "./routes/home/home.component";
 import Authenticating from "./routes/authenticating/authenticating.component";
+import Shop from "./routes/shop/shop.component";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
             <Route path={'/'} element={<Navigation />}>
                 <Route index element={<HomePage />} />
+                <Route path={'shop'} element={<Shop />} />
                 <Route path={'auth'} element={<Authenticating />} />
             </Route>
         </Routes>
